c93a162a77cccd7deb42f52ee3fb8f05e33e2f90
Ardi Germenji <ardi.germenji@interlogica.it>
2023-11-26 11:58:27 +0100

    bare implmentation of carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers'
 import { createServerClient } from './supabase/server'
 import Gallery from '@/components/images/gallery'
+import { Carousel } from '@/components/images/carousel'
 
 export default async function Home() {
   const cookieStore = cookies()
@@ -8,6 +9,7 @@ export default async function Home() {
   const { data, error } = await supabase.from('about').select('*')
   return (
     <div className="min-h-screenitems-center flex flex-col justify-between p-24">
+      <Carousel creations={data ?? []} />
       <Gallery creations={data ?? []} />
     </div>
   )
--- components/images/carousel.tsx
+++ components/images/carousel.tsx
@@ -1,13 +1,35 @@
+'use client'
 import { ApiCreation } from '@/app/supabase/types'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export function Carousel({ creations }: { creations: ApiCreation[] }) {
+  const [active, setActive] = useState(0)
+
+  useEffect(() => {
+    const intervallo = setInterval(() => {
+      setActive((active + 1) % creations.length)
+    }, 3000) // Cambia ogni 3 secondi
+    return () => clearInterval(intervallo)
+  }, [active, creations.length])
+
   return (
-    <div className="mx-auto">
-      <div className="carousel">
-        {creations.map((creation) => (
-          <BlurImage key={creation.id} creation={creation} />
-        ))}
-      </div>
+    <div className="mx-auto flex ">
+      {creations.map(({ src, id }, idx) => (
+        <Image
+          key={id}
+          alt={`${id} picture`}
+          src={src}
+          priority={true}
+          width={700}
+          height={700}
+          className={twMerge(
+            'duration-700 ease-in-out',
+            active === idx ? 'opacity-100' : 'opacity-0',
+          )}
+        />
+      ))}
     </div>
   )
 }
