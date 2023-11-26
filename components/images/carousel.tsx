cbdd919dacb5189d5e0dbd31847c1ae33760dc74
Ardi Germenji <ardi.germenji@interlogica.it>
2023-11-26 11:25:37 +0100

    Remove commented out code and update image domain pattern

---
+++ components/images/carousel.tsx
@@ -0,0 +1,11 @@
+export function Carousel({ creations }: { creations: ApiCreation[] }) {
+  // return (
+  //   <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+  //     <div className="carousel">
+  //       {creations.map((creation) => (
+  //         <BlurImage key={creation.id} creation={creation} />
+  //       ))}
+  //     </div>
+  //   </div>
+  // )
+}
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['uvogrrsrsvbxweesxnho.supabase.co'],
+    remotePatterns: ['uvogrrsrsvbxweesxnho.supabase.co'],
   },
 }
 
