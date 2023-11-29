4601e5cfe0d021c4289a0cb41191e7af1eea8bc2
Ardi Germenji <ardi.germenji@interlogica.it>
2023-11-29 10:20:08 +0100

    Refactor contact form styling

--- components/contact-from.tsx
+++ components/contact-from.tsx
@@ -38,37 +38,60 @@ const ContactFrom = () => {
   }
 
   return (
-    <div>
-      <h1>Contattaci</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nome:</label>
+    <div className="justify- flex min-h-screen flex-col items-center">
+      <h1 className="mb-5 text-2xl font-bold">Contattaci</h1>
+      {error && <p className="text-red-500">{error}</p>}
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="mb-2 block text-sm font-bold text-gray-700"
+          >
+            Nome:
+          </label>
           <input
             type="text"
             name="name"
             value={form.name}
             onChange={handleChange}
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
           />
         </div>
-        <div>
-          <label htmlFor="replyTo">Email:</label>
+        <div className="mb-4">
+          <label
+            htmlFor="replyTo"
+            className="mb-2 block text-sm font-bold text-gray-700"
+          >
+            Email:
+          </label>
           <input
             type="email"
             name="replyTo"
             value={form.replyTo}
             onChange={handleChange}
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
           />
         </div>
-        <div>
-          <label htmlFor="message">Messaggio:</label>
+        <div className="mb-6">
+          <label
+            htmlFor="message"
+            className="mb-2 block text-sm font-bold text-gray-700"
+          >
+            Messaggio:
+          </label>
           <textarea
             name="message"
             value={form.message}
             onChange={handleChange}
+            className="focus:shadow-outline h-20 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
           ></textarea>
         </div>
-        <button type="submit">Invia</button>
+        <button
+          type="submit"
+          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+        >
+          Invia
+        </button>
       </form>
     </div>
   )
