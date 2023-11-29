1224c0a21e2e05bbd94dd6f9cc5cdc88fb986bcd
Ardi Germenji <ardi.germenji@interlogica.it>
2023-11-29 10:34:48 +0100

    Add link component and success message

--- components/contact-from.tsx
+++ components/contact-from.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { useState } from 'react'
 
 const ContactFrom = () => {
@@ -58,67 +59,78 @@ const ContactFrom = () => {
   return (
     <div className="justify- flex min-h-screen flex-col items-center">
       <h1 className="mb-5 text-2xl font-bold">Contattaci</h1>
+
       {error && <p className="text-red-500">{error}</p>}
-      {success && (
-        <p className="text-green-500">
-          Il tuo messaggio è stato inviato con successo!
-        </p>
+      {success ? (
+        <div className="flex flex-col gap-6">
+          <p className="text-green-500">
+            Il tuo messaggio è stato inviato con successo!
+          </p>
+          <Link
+            href="/contact"
+            className="mt-6focus:shadow-outline rounded bg-blue-500 px-4 py-2
+            text-center font-bold text-white hover:bg-blue-700 focus:outline-purple-500"
+          >
+            Invia un altro messaggio
+          </Link>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="w-full max-w-md">
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="mb-2 block text-sm font-bold text-gray-700"
+            >
+              Nome:
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="replyTo"
+              className="mb-2 block text-sm font-bold text-gray-700"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              name="replyTo"
+              value={form.replyTo}
+              onChange={handleChange}
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="mb-2 block text-sm font-bold text-gray-700"
+            >
+              Messaggio:
+            </label>
+            <textarea
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              className="focus:shadow-outline h-20 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className={`focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-purple-500 ${
+              loading ? 'cursor-not-allowed opacity-50' : ''
+            }`}
+          >
+            {loading ? 'Invio...' : 'Invia'}
+          </button>
+        </form>
       )}
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="mb-2 block text-sm font-bold text-gray-700"
-          >
-            Nome:
-          </label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="replyTo"
-            className="mb-2 block text-sm font-bold text-gray-700"
-          >
-            Email:
-          </label>
-          <input
-            type="email"
-            name="replyTo"
-            value={form.replyTo}
-            onChange={handleChange}
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="message"
-            className="mb-2 block text-sm font-bold text-gray-700"
-          >
-            Messaggio:
-          </label>
-          <textarea
-            name="message"
-            value={form.message}
-            onChange={handleChange}
-            className="focus:shadow-outline h-20 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-purple-500"
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className={`focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-purple-500 ${
-            loading ? 'cursor-not-allowed opacity-50' : ''
-          }`}
-        >
-          {loading ? 'Invio...' : 'Invia'}
-        </button>
-      </form>
     </div>
   )
 }
