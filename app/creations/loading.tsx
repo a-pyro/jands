fae7d70fd1e8767d9803c777b2cf64cafa06a38c
a-pyro <ardi.germenji@gmail.com>
2023-12-03 12:34:34 +0100

    Update image paths in loading component

---
+++ app/creations/loading.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import Image1 from '../../public/loading/1.jpg'
+import Image2 from '../../public/loading/2.jpg'
+import Image3 from '../../public/loading/3.jpg'
+import Image4 from '../../public/loading/4.jpg'
+import Image5 from '../../public/loading/5.jpg'
+import Image6 from '../../public/loading/6.jpg'
+import Image7 from '../../public/loading/7.jpg'
+import Image8 from '../../public/loading/8.jpg'
+import Image9 from '../../public/loading/9.jpg'
+import Image10 from '../../public/loading/10.jpg'
+import Image11 from '../../public/loading/11.jpg'
+import Image12 from '../../public/loading/12.jpg'
+import Image13 from '../../public/loading/13.jpg'
+
+const images = [
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+]
+
+const getRandomImage = () => {
+  const random = Math.floor(Math.random() * images.length)
+  return images[random]
+}
+
+export default function Loading() {
+  return (
+    <Image
+      src={getRandomImage()}
+      alt="Loading..."
+      fill
+      className="z-50 object-cover object-center"
+    />
+  )
+}
