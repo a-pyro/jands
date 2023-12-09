4fb7d43f388cefb592943cc04bf38bb4a2ab9564
Ardi Germenji <ardi.germenji@interlogica.it>
2023-12-09 15:43:37 +0100

    Refactor about page and remove unused images

--- app/[locale]/(customer)/about/page.tsx
+++ app/[locale]/(customer)/about/page.tsx
@@ -1,42 +1,67 @@
-// import CreationPage from '../_shared/shared-page'
+/* eslint-disable @next/next/no-img-element */
+import { useTranslations } from 'next-intl'
 
 export default function About() {
+  const t = useTranslations('about')
+  const introduction = t('introduction')
+  const unique_creations = t('unique_creations')
+  const variety = t('variety')
+  const founders = t('founders')
+  const friendship = t('friendship')
+
   return (
     <div>
-      <div className="flex flex-wrap p-4 md:flex-nowrap">
-        {/* // <!-- Sezione di introduzione --> */}
-        <div className="w-full md:w-1/2">
-          <p>Testo introduttivo qui...</p>
+      <div className="flex">
+        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{introduction}</p>
+        <div className="overflow-hidden">
+          <img
+            src="/about/work-outside.jpg"
+            alt="Work Outside"
+            className="transition-transform duration-300 hover:scale-105"
+          />
         </div>
-        <div className="w-full md:w-1/2">
-          {/* <!-- Immagine landscape qui --> */}
+      </div>
+      <div className="flex">
+        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">
+          {unique_creations}
+        </p>
+        <div className="overflow-hidden">
+          <img
+            src="/about/work-table.jpg"
+            alt="Work Table"
+            className="transition-transform duration-300 hover:scale-105"
+          />
         </div>
       </div>
 
-      {/* // <!-- Sezione "La Nostra Storia" --> */}
-      <div className="p-4">
-        <div className="flex flex-wrap">
-          <div className="w-full md:w-1/2">
-            <p>Primo paragrafo della storia...</p>
-            {/* <!-- Immagine portrait qui --> */}
-          </div>
-          <div className="w-full md:w-1/2">
-            <p>Secondo paragrafo della storia...</p>
-            {/* <!-- Immagine portrait qui --> */}
-          </div>
+      <div className="flex">
+        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{variety}</p>
+        <div className="overflow-hidden">
+          <img
+            src="/about/vetrina.jpg"
+            alt="Vetrina"
+            className="transition-transform duration-300 hover:scale-105"
+          />
         </div>
       </div>
-
-      {/* // <!-- Galleria di Creazioni --> */}
-      <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-4">
-        {/* <!-- Immagini portrait qui --> */}
+      <div className="flex">
+        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{founders}</p>
+        <div className="overflow-hidden">
+          <img
+            src="/about/vetrina-smile.jpg"
+            alt="Vetrina Smile"
+            className="transition-transform duration-300 hover:scale-105"
+          />
+        </div>
       </div>
-
-      {/* // <!-- Filosofia e Visione --> */}
-      <div className="flex flex-wrap p-4">
-        <div className="w-full">
-          <p>Filosofia e visione...</p>
-          {/* <!-- Elementi grafici/icone qui --> */}
+      <div className="flex">
+        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{friendship}</p>
+        <div className="overflow-hidden">
+          <img
+            src="/about/hug-smile.jpg"
+            alt="Hug Smile"
+            className="transition-transform duration-300 hover:scale-105"
+          />
         </div>
       </div>
     </div>
