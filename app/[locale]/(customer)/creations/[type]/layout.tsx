32f119d01cb337b03936bd11a87a6b95e8e86ee9
Ardi Germenji <ardi.germenji@interlogica.it>
2023-12-09 12:46:38 +0100

    Refactor metadata generation in layout.tsx to call translations with namespace

--- app/[locale]/(customer)/creations/[type]/layout.tsx
+++ app/[locale]/(customer)/creations/[type]/layout.tsx
@@ -15,11 +15,12 @@ export async function generateMetadata({
 }: MetadataProps) {
   const t = await getTranslations({
     locale,
+    namespace: 'creations',
   })
 
   return {
-    title: `${t('creations.metadata.title')} ${t(`creations.${type}.title`)}`,
-    description: t(`creations.${type}.description`),
+    title: `${t('metadata.title')} ${t(`${type}.title`)}`,
+    description: t(`${type}.description`),
   }
 }
 
