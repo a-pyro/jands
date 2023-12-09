4218409156e5618fb626ca782d6dd0334c4fa30a
Ardi Germenji <ardi.germenji@interlogica.it>
2023-12-09 13:14:53 +0100

    Refactor shared-page.tsx to use next-intl for translations

--- app/_shared/shared-page.tsx
+++ app/_shared/shared-page.tsx
@@ -1,18 +1,19 @@
 import GridGallery from '@/components/gallery/grid-gallery'
 import { type CreationType } from '../../services/types'
 import getResults from '@/services/getResults'
-import { dictionary } from '@/messages/dictionary'
+import { getTranslations } from 'next-intl/server'
 
 export default async function CreationPage({
   creationType,
 }: {
-  creationType: CreationType | 'about'
+  creationType: CreationType
 }) {
   const result = await getResults({ folderName: creationType, limit: 100 })
+  const t = await getTranslations('creations')
 
   return (
     <div className="flex flex-col">
-      <h1 className="mb-5 font-medium">{dictionary[creationType]}</h1>
+      <h1 className="mb-5 font-medium">{t(`${creationType}.title`)}</h1>
       <GridGallery images={result?.resources ?? []} />
     </div>
   )
