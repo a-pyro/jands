3365e6c98a0217e688be8ebfac6e45e138f472e5
Ardi Germenji <ardi.germenji@interlogica.it>
2023-12-09 15:59:59 +0100

    add about components

--- app/[locale]/(customer)/about/page.tsx
+++ app/[locale]/(customer)/about/page.tsx
@@ -1,6 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import { useTranslations } from 'next-intl'
 
+const TextParagraph = ({ children }: { children: string }) => (
+  <p className="mb-4 text-xl md:w-1/2 md:text-2xl lg:text-3xl">{children}</p>
+)
+
+const Image = ({ src, alt }: { src: string; alt: string }) => (
+  <div className="w-full overflow-hidden md:w-1/2">
+    <img
+      src={src}
+      alt={alt}
+      className="transition-transform duration-300 ease-in-out hover:scale-105"
+    />
+  </div>
+)
+
+const BlockWrapper = ({ children }: { children: React.ReactNode }) => (
+  <div className="my-6 flex flex-col items-center md:flex-row md:space-x-6">
+    {children}
+  </div>
+)
+
 export default function About() {
   const t = useTranslations('about')
   const introduction = t('introduction')
@@ -10,60 +30,31 @@ export default function About() {
   const friendship = t('friendship')
 
   return (
-    <div>
-      <div className="flex">
-        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{introduction}</p>
-        <div className="overflow-hidden">
-          <img
-            src="/about/work-outside.jpg"
-            alt="Work Outside"
-            className="transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-      </div>
-      <div className="flex">
-        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">
-          {unique_creations}
-        </p>
-        <div className="overflow-hidden">
-          <img
-            src="/about/work-table.jpg"
-            alt="Work Table"
-            className="transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-      </div>
+    <div className="container mx-auto p-6">
+      <BlockWrapper>
+        <TextParagraph>{introduction}</TextParagraph>
+        <Image src="/about/work-outside.jpg" alt="Work Outside" />
+      </BlockWrapper>
 
-      <div className="flex">
-        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{variety}</p>
-        <div className="overflow-hidden">
-          <img
-            src="/about/vetrina.jpg"
-            alt="Vetrina"
-            className="transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-      </div>
-      <div className="flex">
-        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{founders}</p>
-        <div className="overflow-hidden">
-          <img
-            src="/about/vetrina-smile.jpg"
-            alt="Vetrina Smile"
-            className="transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-      </div>
-      <div className="flex">
-        <p className="mb-4 text-xl md:text-2xl lg:text-3xl">{friendship}</p>
-        <div className="overflow-hidden">
-          <img
-            src="/about/hug-smile.jpg"
-            alt="Hug Smile"
-            className="transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-      </div>
+      <BlockWrapper>
+        <Image src="/about/work-table.jpg" alt="Work Table" />
+        <TextParagraph>{unique_creations}</TextParagraph>
+      </BlockWrapper>
+
+      <BlockWrapper>
+        <TextParagraph>{variety}</TextParagraph>
+        <Image src="/about/vetrina.jpg" alt="Vetrina" />
+      </BlockWrapper>
+
+      <BlockWrapper>
+        <Image src="/about/vetrina-smile.jpg" alt="Vetrina Smile" />
+        <TextParagraph>{founders}</TextParagraph>
+      </BlockWrapper>
+
+      <BlockWrapper>
+        <TextParagraph>{friendship}</TextParagraph>
+        <Image src="/about/hug-smile.jpg" alt="Hug Smile" />
+      </BlockWrapper>
     </div>
   )
 }
