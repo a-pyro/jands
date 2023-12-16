dae597e0caaee155d521f66aeb420b31a10ac8d8
a-pyro <ardi.germenji@gmail.com>
2023-12-16 04:44:23 +0100

    Update delaysClasses and threshold value in Intersecting component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,25 +1,26 @@
-import { twJoin } from 'tailwind-merge'
+import { ClassNameValue, twJoin } from 'tailwind-merge'
 
 import { SlideGallery } from '@/components/gallery/slide-gallery'
 import { Intersecting } from '@/components/intersecting'
 import { ScrollAdaptiveWrapper } from '@/components/scroll-adaptive-wrapper'
 import { ALL_CREATIONS_TYPES } from '@/services/types'
 
-// const delaysClasses = [
-//   'delay-100',
-//   'delay-200',
-//   'delay-300',
-//   'delay-400',
-//   'delay-500',
-// ]
+const delaysClasses: ClassNameValue = [
+  'delay-500',
+  'delay-700',
+  'delay-300',
+  'delay-200',
+  'delay-100',
+]
 
 const Home = () => {
   return (
-    <ScrollAdaptiveWrapper className="flex flex-col gap-20">
-      {ALL_CREATIONS_TYPES.map((creationType) => (
+    <ScrollAdaptiveWrapper className="dela flex flex-col gap-20">
+      {ALL_CREATIONS_TYPES.map((creationType, idx) => (
         <Intersecting
           classNameInView={twJoin(
             'opacity-100 translate-y-0 transition-all duration-500',
+            delaysClasses[idx],
           )}
           classNameNotInView="opacity-0 translate-y-10"
           key={creationType}
