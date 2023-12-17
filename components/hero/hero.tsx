266c115e8f9eaa95e0ab358678149719a36bd4f9
a-pyro <ardi.germenji@gmail.com>
2023-12-17 12:11:11 +0100

    Update Hero component styling

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -8,7 +8,7 @@ import { Description, Subtitle, Title } from './text'
 
 export const Hero = () => {
   return (
-    <div className="flex h-[calc(100dvh-100px)] flex-col place-content-center items-center justify-center gap-6 px-10 text-center">
+    <div className="flex h-[calc(100dvh-100px)] flex-col place-content-center items-center justify-center gap-6 px-10 pb-32 text-center">
       <Title text="Arte che risuona con la Natura" />
       <Subtitle text="Scopri l'unicità dell'arte fatta a mano" />
       <Description text="Immergiti in un mondo dove ogni pezzo racconta una storia unica - una fusione tra natura, creatività e amore." />
